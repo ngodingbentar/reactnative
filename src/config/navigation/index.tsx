46165776e1432e5b02c3d45775e3547687a78d1f
wadudu01 <deqin.ig@gmail.com>
2020-07-11 15:31:35 +0700

    icon vector

--- src/config/navigation/index.tsx
+++ src/config/navigation/index.tsx
@@ -5,7 +5,8 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MapScreen from '../../containers/screens/Map';
 import CountryScreen from '../../containers/screens/Country';
 import MainStack from './MainStack';
-
+import color from '../constant/color';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
@@ -13,8 +14,22 @@ const Tab = createBottomTabNavigator();
 const RootNavigation = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Main" component={MainStack} />
+      <Tab.Navigator 
+        tabBarOptions={{
+          style:{
+            backgroundColor: color.oren,
+            activeTintColor: color.teal,
+            // activeBackgroundColor: color.blackLight,
+          },
+        }}
+      >
+        <Tab.Screen 
+          name="Main" 
+          component={MainStack} 
+          options={{
+            tabBarIcon:()=> <Icon name="md-apps" size={30} />
+          }}
+          />
         <Tab.Screen name="Map" component={MapScreen} />
         <Tab.Screen name="Country" component={CountryScreen} />
       </Tab.Navigator>
